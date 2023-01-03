07124d6b62da0259030aaf7c7e278f4d2921954a
Bawdy Ink Slinger <51732963+BawdyInkSlinger@users.noreply.github.com>
2023-01-03 03:09:13 -0800

    Additional logging

--- previous-next.js
+++ previous-next.js
@@ -18,10 +18,12 @@ previousNext = function (when, previousActionOrSelector, nextActionOrSelector) {
                 console.log("keydown event", e, "previousActionOrSelector", previousActionOrSelector, "nextActionOrSelector", nextActionOrSelector);
                 // left arrow
                 if ((e.keyCode || e.which) == 37) {
+                    console.log("Perform action on previousActionOrSelector");
                     performAction(previousActionOrSelector);
                 }
                 // right arrow
                 if ((e.keyCode || e.which) == 39) {
+                    console.log("Perform action on nextActionOrSelector");
                     performAction(nextActionOrSelector);
                 }
             });
